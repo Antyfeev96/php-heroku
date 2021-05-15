f10fe640a8ff718d924f0022677f972b7d43f062
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-05-15 15:31:40 +0300

    Redirect message added

--- web/index.php
+++ web/index.php
@@ -1,2 +1,7 @@
 <?php
 include 'index.html';
+if (isset($_SESSION['username'])) {
+  echo 'Hello, ' . $_SESSION['username'];
+} else {
+  echo '123';
+}
--- web/post.php
+++ web/post.php
@@ -3,4 +3,4 @@ echo $_POST['username'];
 session_start();
 $redirect = $_SERVER['HTTP_REFERER'];
 $_SESSION['username'] = $_POST['username'];
-header("Location: $redirect, true, 301");
+header("Location: $redirect", true, 301);
